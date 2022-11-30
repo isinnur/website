a20546b98c95ca81bbc41cb353451c72761a6eb0
isinnur <isinngnyy@gmail.com>
2022-11-30 22:19:30 +0300

    add skills

---
+++ script.js
@@ -0,0 +1,42 @@
+// document.getElementById("theme-change").addEventListener('click' , () => {
+    // document.body.classList.toggle('dark-theme');
+    // if()
+// });
+
+// const dark = document.getElementById("theme-change");
+
+// dark.addEventListener("click" ,() => {
+// dark.classList.to
+// });
+
+
+
+
+
+
+
+// function changetema(){
+    // document.querySelector("body").style.backgroundColor ="#111";
+// }
+
+// const theme = document.getElementById("theme-change");
+// theme.addEventListener("click",() =>{
+    // document.querySelector("body").style.backgroundColor ="#111";
+    // document.querySelector("body").style.color ="#fff";
+// });
+
+
+// const theme = document.getElementById("theme-change");
+// theme.addEventListener("click", change);
+
+// function change(){
+    // document.querySelector("body").style.backgroundColor="#111";
+// },
+
+const toggle = document.getElementById("theme-change");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click",function(){
+   body.style.backgroundColor="black";
+   body.style.color="black";
+});
