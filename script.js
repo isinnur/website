f3873cd962b28c90b04c2b52990e1d0324240be9
isinnur <isinngnyy@gmail.com>
2022-11-30 23:53:32 +0300

    add light/dark mode

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ function myFunction(){
     var header = document.querySelector(".menu");
     header.classList.toggle("dark-mode");
 }
-
- const theme = document.getElementById("theme-change");
- theme.addEventListener("click",() =>{
- document.querySelector(".header").style.color ="#fff";
-    // document.querySelector("body").style.color ="#fff";
- });
