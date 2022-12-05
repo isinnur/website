097704906800c76731b38ec98bbae881c52fe951
isinnur <isinngnyy@gmail.com>
2022-12-05 13:49:58 +0300

    feat: add menu bars

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function myFunction(){
     var element = document.body;
     element.classList.toggle("dark-mode");
+}
 
-    var header = document.querySelector(".menu");
-    header.classList.toggle("dark-mode");
+
+const about = document.querySelector(".about");
+about.addEventListener("click",func());
+
+
+
+function gelcekMenu(){
+    var menu = document.querySelector('.gelcek');
+    var navigation = document.querySelector('.navigation');
+    menu.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
